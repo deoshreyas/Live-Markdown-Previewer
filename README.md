c0d0d17effec6df7bdd3af043dd0f99069e1b42f
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-11 00:07:52 +0530

    Fix preview bug due to use of textarea, Change to div element, Add options to save and download files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Live-Markdown-Previewer
-A website to preview Markdown Files live!
+
+Live @ https://deoshreyas.github.io/Live-Markdown-Previewer/
+
+## :zap: About
+A fun little website to get live previews of Markdown (.md) files, with options to save and create new ones!
+
+## :desktop_computer: How to run 
+Just click [here](https://deoshreyas.github.io/Live-Markdown-Previewer/)!
--- script.js
+++ script.js
@@ -4,3 +4,29 @@ document.getElementById('markdown-text').addEventListener('input', function() {
     var clean_markdown = DOMPurify.sanitize(converted);
     document.getElementById('markdown-preview').innerHTML = clean_markdown;
 });
+
+function readFileContents() {
+    document.getElementById('markdownFile').click();
+}
+
+document.getElementById('markdownFile').addEventListener("change", function() {
+    var mdFile = this.files[0];
+    var reader = new FileReader();
+    reader.readAsText(mdFile);
+    reader.onload = function() {
+        document.getElementById('markdown-text').value = reader.result;
+        // manually trigger the change event so the markdown text is rendered
+        document.getElementById('markdown-text').dispatchEvent(new Event('input'));
+    };
+});
+
+function saveFileContents() {
+    var textToSave = document.getElementById('markdown-text').value;
+    var blobFile = new Blob([textToSave], {type: "text/plain"});
+    var url = URL.createObjectURL(blobFile);
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = "markdown.md";
+    a.click();
+    URL.revokeObjectURL(url);
+}
